7383bdf49cf30875c176737cbe02c998c39b9e6b
Dyako Baram <55919885+dyako-baram@users.noreply.github.com>
2021-04-24 16:19:42 +0300

    finished

--- SignalRChat/wwwroot/js/chat.js
+++ SignalRChat/wwwroot/js/chat.js
@@ -1,29 +1,46 @@
 "use strict";
-
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
-
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
-
 connection.on("ReceiveMessage", function (user, message) {
-    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
-    var li = document.createElement("li");
-    li.textContent = encodedMsg;
-    document.getElementById("messagesList").appendChild(li);
+    if(user!=""){
+        var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+        var encodedMsg = user + ": " + msg;
+        var li = document.createElement("li");
+        li.textContent = encodedMsg;
+        document.getElementById("messagesList").appendChild(li);
+    }
+    
 });
-
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
-
+//send message when pressing send button
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", user, message).catch(function (err) {
+    let message = document.getElementById("messageInput");
+    if(message.value !=""){
+       connection.invoke("SendMessage", user, message.value).catch(function (err) {
         return console.error(err.toString());
-    });
+        });
+        message.value="" 
+    }
+    
     event.preventDefault();
+});
+
+//send message when pressing enter
+window.addEventListener("keydown", function (event) {
+    var user = document.getElementById("userInput").value;
+    let message = document.getElementById("messageInput");
+    if(event.key=="Enter" && message.value !="")
+    {
+        connection.invoke("SendMessage", user, message.value).catch(function (err) {
+            return console.error(err.toString());
+        });
+        message.value=""
+        event.preventDefault();
+    }
 });
